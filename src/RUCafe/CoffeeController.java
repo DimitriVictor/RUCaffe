c02dec524ac0f9383d30e2ddc5cc9914b40174a1
dimitrivictor1217@gmail.com <dimitrivictor1217@gmail.com>
2021-03-31 00:54:40 -0400

    Coffee controller, and cofee.fxml

--- src/RUCafe/CoffeeController.java
+++ src/RUCafe/CoffeeController.java
@@ -1,4 +1,28 @@
 package RUCafe;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.util.Observable;
 
 public class CoffeeController {
+
+    @FXML
+    public ComboBox sizeComboBox;
+
+    @FXML
+    public ComboBox countComboBox;
+
+
+    @FXML
+    public void initialize() {
+        ObservableList<String> sizes = FXCollections.observableArrayList("Small","Medium", "Large");
+        sizeComboBox.setItems(sizes);
+
+        ObservableList<Integer> numbers = FXCollections.observableArrayList(1,2,3);
+        countComboBox.setItems(numbers);
+
+    }
+
 }
