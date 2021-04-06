1a710b41b30238d4a8fa588b42743ebda4518670
PA200 <padmank.mail@gmail.com>
2021-04-05 22:19:59 -0400

    [ADD] Add to Order Button Done

--- src/RUCafe/Constants.java
+++ src/RUCafe/Constants.java
@@ -11,6 +11,9 @@ public class Constants {
     public static final int DONUT_WINDOW_HEIGHT = 450;
     public static final int DONUT_WINDOW_WIDTH = 500;
 
+    //CONSTANTS FOR PRICING
+    public static final double SALES_TAX = 6.625;
+
     //CONSTANTS FOR DONUT OBJECT
     public static final double YEAST_DONUT_PRICE = 1.39;
     public static final double CAKE_DONUT_PRICE = 1.59;
