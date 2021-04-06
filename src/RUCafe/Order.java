a1d711441b3d5e3325d98deb78e5e5cd95566709
dimitrivictor1217@gmail.com <dimitrivictor1217@gmail.com>
2021-04-06 00:22:07 -0400

    Merge branch 'master' of https://github.com/PA2000/SoftwareMethProject4
     Conflicts:
    	out/production/SoftwareMethProject4/RUCafe/MainMenuController.class

--- src/RUCafe/Order.java
+++ src/RUCafe/Order.java
@@ -55,4 +55,5 @@ public class Order implements Customizable{
         }
         return list;
     }
+
 }
--- src/RUCafe/StoreOrderController.java
+++ src/RUCafe/StoreOrderController.java
@@ -1,6 +1,46 @@
 package RUCafe;
 
+import java.util.ArrayList;
+import java.util.Observable;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class StoreOrderController implements Customizable{
+    public static ArrayList<Order> storeOrders = new ArrayList<>();
+
+    @FXML
+    private ListView listView;
+
+    @FXML
+    private Button exportOrderButton;
+
+    @FXML
+    private Button cancelOrderButton;
+
+    @FXML
+    private TextField totalTextField;
+
+    @FXML
+    private ComboBox chooseStoreOrderComboBox;
+
+    @FXML
+    public void initialize() {
+        ObservableList<Integer> orders = FXCollections.observableArrayList();
+        for(int i = 0; i < storeOrders.size(); i++){
+            orders.add(i);
+        }
+
+        ObservableList<String> sizes = FXCollections.observableArrayList(Constants.SMALL,Constants.MEDIUM, Constants.LARGE);
+
+        chooseStoreOrderComboBox.setItems(sizes);
+
+        ObservableList<Integer> numbers = FXCollections.observableArrayList(Constants.ONE,Constants.TWO,Constants.THREE);
+        chooseStoreOrderComboBox.setItems(numbers);
+
+    }
 
     @Override
     public boolean add(Object obj) {
@@ -11,4 +51,15 @@ public class StoreOrderController implements Customizable{
     public boolean remove(Object obj) {
         return false;
     }
+
+    public void displayWarning(String warningMessage){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("WARNING");
+        alert.setHeaderText("Invalid!");
+        alert.setContentText(warningMessage);
+        alert.showAndWait();
+    }
+
+
+
 }
